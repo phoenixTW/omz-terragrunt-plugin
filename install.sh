8f275af626980f23a03e040d838a32efcb99bc92
Kaustav Chakraborty <kaustav.ron@gmail.com>
2022-09-13 00:04:58 +0530

    Kaustav | Updated banner.

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 # #!/usr/bin/env bash
 
-OH_MY_ZSH_PLUGINS=$HOME/.oh-my-zsh/plugins
-current_timestamp=$(date "+%Y%m%d-%H%M%S")
-TEMP_DIR="${HOME}/.tmp-${current_timestamp}"
-TERRAGRUNT_PLUGIN_DIR="${TEMP_DIR}/terragrunt-plugin"
+local OH_MY_ZSH_PLUGINS=$HOME/.oh-my-zsh/plugins
+local current_timestamp=$(date "+%Y%m%d-%H%M%S")
+local TEMP_DIR="${HOME}/.tmp-${current_timestamp}"
+local TERRAGRUNT_PLUGIN_DIR="${TEMP_DIR}/terragrunt-plugin"
 
 echo "
- __                       __  __                
-/  \ |__|     |\/| \ /     / /__` |__|          
-\__/ |  |     |  |  |     /_ .__/ |  |          
-                                                
-___  ___  __   __        __   __            ___ 
- |  |__  |__) |__)  /\  / _` |__) |  | |\ |  |  
- |  |___ |  \ |  \ /~~\ \__> |  \ \__/ | \|  |  
-                                                
+ .d88888b.  888    888      888b     d888 Y88b   d88P      8888888888P  .d8888b.  888    888                        
+d88P" "Y88b 888    888      8888b   d8888  Y88b d88P             d88P  d88P  Y88b 888    888                        
+888     888 888    888      88888b.d88888   Y88o88P             d88P   Y88b.      888    888                        
+888     888 8888888888      888Y88888P888    Y888P             d88P     "Y888b.   8888888888                        
+888     888 888    888      888 Y888P 888     888             d88P         "Y88b. 888    888                        
+888     888 888    888      888  Y8P  888     888            d88P            "888 888    888                        
+Y88b. .d88P 888    888      888   "   888     888           d88P       Y88b  d88P 888    888                        
+ "Y88888P"  888    888      888       888     888          d8888888888  "Y8888P"  888    888                        
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+88888888888 8888888888 8888888b.  8888888b.         d8888  .d8888b.  8888888b.  888     888 888b    888 88888888888 
+    888     888        888   Y88b 888   Y88b       d88888 d88P  Y88b 888   Y88b 888     888 8888b   888     888     
+    888     888        888    888 888    888      d88P888 888    888 888    888 888     888 88888b  888     888     
+    888     8888888    888   d88P 888   d88P     d88P 888 888        888   d88P 888     888 888Y88b 888     888     
+    888     888        8888888P"  8888888P"     d88P  888 888  88888 8888888P"  888     888 888 Y88b888     888     
+    888     888        888 T88b   888 T88b     d88P   888 888    888 888 T88b   888     888 888  Y88888     888     
+    888     888        888  T88b  888  T88b   d8888888888 Y88b  d88P 888  T88b  Y88b. .d88P 888   Y8888     888     
+    888     8888888888 888   T88b 888   T88b d88P     888  "Y8888P88 888   T88b  "Y88888P"  888    Y888     888     
+                                                                                                                    
+                                                                                                                    
 "
 echo "Developed and maintained by "$(tput bold)"Kaustav Chakraborty (https://github.com/phoenixTW)\n\n\n"$(tput sgr 0)
 
